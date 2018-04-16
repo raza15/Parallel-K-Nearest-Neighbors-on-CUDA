3b1782dfb01ab3a21525e90194c7ec97b1a11d81
raza15 <16100251@lums.edu.pk>
2018-04-16 15:13:50 -0400

    basic functions written

--- knn/serial.c
+++ knn/serial.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int ROWS = 3;
-const int COLS = 3;
+const int USERS = 3;
+const int ATTRIBUTES = 5;
+const int K = 1;
 
-void printMatrix(int matrix[ROWS][COLS]) {
+void printMatrix(int matrix[USERS][ATTRIBUTES]) {
 	int row; int col;
-	for(row = 0; row < ROWS; row++) {
-		for(col = 0; col < COLS; col++) {
+	printf("Matrix:-\n");
+	for(row = 0; row < USERS; row++) {
+		for(col = 0; col < ATTRIBUTES; col++) {
 			printf("%d, ", matrix[row][col]);
 		}
 		printf("\n");
 	}
 }
 
-void populateMatrix(const char * fileName, int matrix[ROWS][COLS]) {
+void printScores(int matrix[USERS][USERS]) {
+        int row; int col;
+	printf("Scores:-\n");
+        for(row = 0; row < USERS; row++) {
+                for(col = 0; col < USERS; col++) {
+                        printf("%d, ", matrix[row][col]);
+                }
+                printf("\n");
+        }
+}
+
+void readDataFromFile(const char * fileName, int matrix[USERS][ATTRIBUTES]) {
 	FILE * fp;  
         fp = fopen(fileName, "r+");
         int row; int col;
-        for(row = 0; row < ROWS; row++) {
-                for(col = 0; col < COLS; col++) {
+        for(row = 0; row < USERS; row++) {
+                for(col = 0; col < ATTRIBUTES; col++) {
                         fscanf(fp, "%d", &matrix[row][col]);
                 }
         }
 }
 
+void calculateScores(int matrix[USERS][ATTRIBUTES], int scores[USERS][USERS]) {
+
+}
+
 int main() {
-	int matrix[ROWS][COLS];
-	populateMatrix("testData.txt", matrix);
-	printf("Hello World!\n");
+	int matrix[USERS][ATTRIBUTES];
+	readDataFromFile("testData.txt", matrix);
 	printMatrix(matrix);
+	int scores[USERS][USERS];
+	calculateScores(matrix, scores);
+	printScores(scores);
 	return 0;
 }
